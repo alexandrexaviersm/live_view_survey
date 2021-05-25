6ba7250190dd1bb30beb9de2337393748cab222f
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2021-05-24 23:25:28 -0300

    Fix github actions CI

--- config/test.exs
+++ config/test.exs
@@ -12,13 +12,6 @@ config :live_view_survey, LiveViewSurvey.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# Configure the database for GitHub Actions
-if System.get_env("GITHUB_ACTIONS") do
-  config :live_view_survey, LiveViewSurvey.Repo,
-    username: "postgres",
-    password: "postgres"
-end
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :live_view_survey, LiveViewSurveyWeb.Endpoint,
